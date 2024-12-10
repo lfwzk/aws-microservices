37eafe4dfdbde678c69627aed6ba527ad72b1bc4
lfernand_cs <lfernands777@gmail.com>
2024-12-09 21:55:19 -0400

    refactor AWS stack to encapsulate DynamoDB setup in EcommerceDatabase construct

--- lib/aws-microservices-stack.ts
+++ lib/aws-microservices-stack.ts
@@ -1,10 +1,14 @@
-import * as cdk from 'aws-cdk-lib';
-import {Construct} from 'constructs';
-import {AttributeType, BillingMode, Table} from "aws-cdk-lib/aws-dynamodb";
-import {Code, Function, Runtime} from "aws-cdk-lib/aws-lambda";
-import {join} from "path";
-import {NodejsFunction, NodejsFunctionProps} from "aws-cdk-lib/aws-lambda-nodejs";
-import {LambdaRestApi} from "aws-cdk-lib/aws-apigateway";
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import { EcommerceDatabase } from "./database";
 
 // TODO: Investigar sobre Proxys
 
@@ -13,51 +17,41 @@ export class AwsMicroservicesStack extends cdk.Stack {
     super(scope, id, props);
 
     // Aqui va todo nuestra logica de configuracion
-      //dynamoDB
-      const ProductTable = new Table(
-        this, 'product' , {
-            partitionKey: {
-                name: 'id',
-                type: AttributeType.STRING
-            },
-            tableName: 'product',
-            removalPolicy: cdk.RemovalPolicy.DESTROY,
-            billingMode: BillingMode.PAY_PER_REQUEST
+    //dynamoDB
+    const database = new EcommerceDatabase(this, "database");
 
-        });
-      // Lambda
-      const nodeJsFuctionProps: NodejsFunctionProps = {
-          bundling :{
-              externalModules: ['aws-sdk'],
-          },
-          environment: {
-                PRIMARY_KEY: 'id',
-                DYNAMODB_TABLE_NAME: ProductTable.tableName
-          },
-            runtime: Runtime.NODEJS_LATEST
-      }
-      // funtion for product
-      const productFunction = new NodejsFunction(this, 'productLambdaFunction', {
-        entry: join(__dirname, '../src/product/index.ts'),
-        ...nodeJsFuctionProps,
- });
-// grant permission to lambda to access dynamoDB
-      ProductTable.grantReadWriteData(productFunction)
+    // Lambda
+    const nodeJsFuctionProps: NodejsFunctionProps = {
+      bundling: {
+        externalModules: ["aws-sdk"],
+      },
+      environment: {
+        PRIMARY_KEY: "id",
+        DYNAMODB_TABLE_NAME: database.ProductTable.tableName,
+      },
+      runtime: Runtime.NODEJS_LATEST,
+    };
+    // funtion for product
+    const productFunction = new NodejsFunction(this, "productLambdaFunction", {
+      entry: join(__dirname, "../src/product/index.ts"),
+      ...nodeJsFuctionProps,
+    });
+    // grant permission to lambda to access dynamoDB
+    database.ProductTable.grantReadWriteData(productFunction);
     // API_GATEWAY
 
-    const apigateway = new LambdaRestApi(this, 'productApi', {
-        restApiName: 'Product Service',
-        handler: productFunction,
-        proxy: false
+    const apigateway = new LambdaRestApi(this, "productApi", {
+      restApiName: "Product Service",
+      handler: productFunction,
+      proxy: false,
     });
-     const product = apigateway.root.addResource('product');
-     product.addMethod('GET');  // GET /product
-     product.addMethod('POST')  // POST /product
+    const product = apigateway.root.addResource("product");
+    product.addMethod("GET"); // GET /product
+    product.addMethod("POST"); // POST /product
 
-      const productId = product.addResource('{id}');
-        productId.addMethod('GET'); // GET /product/{id}
-        productId.addMethod('PUT'); // PUT /product/{id}
-        productId.addMethod('DELETE'); // DELETE /product/{id}
-
+    const productId = product.addResource("{id}");
+    productId.addMethod("GET"); // GET /product/{id}
+    productId.addMethod("PUT"); // PUT /product/{id}
+    productId.addMethod("DELETE"); // DELETE /product/{id}
   }
 }
