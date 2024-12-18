737c7b0085103db8bdb6a35c976ad93af645c0fe
lfernand_cs <lfernands777@gmail.com>
2024-12-17 22:40:32 -0400

    add basket table to EcommerceDatabase construct for improved product management

--- lib/database.ts
+++ lib/database.ts
@@ -9,10 +9,19 @@ import { Construct } from "constructs";
 
 export class EcommerceDatabase extends Construct {
   public readonly ProductTable: ITable;
+  public readonly basketTable: ITable;
 
   constructor(scope: Construct, id: string) {
     super(scope, id);
+    // Create Product Table
+    this.ProductTable = this.createProductTable();
+    // Create Basket Table
+    this.basketTable = this.createBasketTable();
+  }
+
+  private createProductTable() {
     // Product Table DynamoDB table creation
+    // product: PK: id -- name -- description -- price -- image -- category
     const ProductTable = new Table(this, "product", {
       partitionKey: {
         name: "id",
@@ -22,7 +31,24 @@ export class EcommerceDatabase extends Construct {
       removalPolicy: RemovalPolicy.DESTROY,
       billingMode: BillingMode.PAY_PER_REQUEST,
     });
+    return ProductTable;
+  }
 
-    this.ProductTable = ProductTable;
+  private createBasketTable() {
+    // basket: table DynamoDB table creation
+    // basket: PK : username --items(set-map object)
+    // item1 : {color -- quantity-- price}
+    // item2 : {color -- quantity-- price}
+
+    const basketTable = new Table(this, "basket", {
+      partitionKey: {
+        name: "username",
+        type: AttributeType.STRING,
+      },
+      tableName: " basket",
+      removalPolicy: RemovalPolicy.DESTROY,
+      billingMode: BillingMode.PAY_PER_REQUEST,
+    });
+    return basketTable;
   }
 }
